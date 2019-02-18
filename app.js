adb6c1f4a66eb8e059eb955ae534aad4f60e2fde
A Sama <channelasama@gmail.com>
2019-02-18 19:44:47 +0700

    updatre

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ var updateRouter = require('./routes/update');
 var searchRouter = require('./routes/search');
 var db = "mongodb://asama:12345678a@cluster0-shard-00-00-oszst.mongodb.net:27017,cluster0-shard-00-01-oszst.mongodb.net:27017" +
     ",cluster0-shard-00-02-oszst.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
-var db1 = "mongodb+srv://asama:12345678a@cluster0-oszst.mongodb.net/test?retryWrites=true";
+//var db1 = "mongodb+srv://asama:12345678a@cluster0-oszst.mongodb.net/test?retryWrites=true";
 
 var app = express();
-mongoDB.connect(db1,{ useNewUrlParser: true });
+mongoDB.connect(db,{ useNewUrlParser: true });
 mongoDB.Promise = global.Promise;
 //Lấy kết nối mặc định
-var db1 = mongoDB.connection;
+var db = mongoDB.connection;
 
 //Ràng buộc kết nối với sự kiện lỗi (để lấy ra thông báo khi có lỗi)
-db1.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db1.once('open', function() {
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
     // we're connected!
     console.log("Thanh cong");
 });
